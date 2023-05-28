41205d36887e81abc75bf09cb8ad6db3a94f39e2
xXx_420dankmeme_xXx <ajemtumacz@gmail.com>
2023-05-28 16:41:00 +0200

    Aktualizacja 28.05.2023

--- usa_data.py
+++ usa_data.py
@@ -63,15 +63,80 @@ ax2.set_xlabel('Age')
 ax2.set_ylabel('Number of offenders')
 ax2.set_title('Homicide Age')
 
+#==================Rape Race Data=======================
+df_Rape_Race = pd.read_csv('Offender_Race_05-28-2023_Rape.csv', sep=',')
+df_Rape_Race = pd.DataFrame(df_Rape_Race)
+names_Rape_Race = df_Rape_Race.iloc[:, 0] #Pierwsza kolumna
+data_Rape_Race = df_Rape_Race.iloc[:, 1] #Druga kolumna
+
+fig3, ax3 = plt.subplots()
+ax3.bar(names_Rape_Race, data_Rape_Race)
+ax3.set_xlabel('Race')
+ax3.set_ylabel('Number of offenders')
+ax3.set_title('Rape Race')
+
+#==================Rape Age Data=======================
+df_Rape_Age_Data = pd.read_csv('Offender_Age_05-28-2023_Rape.csv', sep=',')
+df_Rape_Age = pd.DataFrame(df_Rape_Age_Data)
+names_Rape_Age = df_Rape_Age.iloc[:, 0]
+data_Rape_Age = df_Rape_Age.iloc[:, 1]
+
+fig4, ax4 = plt.subplots()
+ax4.bar(names_Rape_Age, data_Rape_Age)
+ax4.set_xlabel('Age')
+ax4.set_ylabel('Number of offenders')
+ax4.set_title('Rape Age')
+
+#==================Robbery Race Data======================
+
+df_Robbery_Race_Data = pd.read_csv('Offender_Race_05-28-2023_Robbery.csv', sep=',')
+df_Robbery_Race = pd.DataFrame(df_Robbery_Race_Data)
+names_Robbery_Race = df_Robbery_Race.iloc[:, 0]
+data_Robbery_Race = df_Robbery_Race.iloc[:, 1]
+
+fig5, ax5 = plt.subplots()
+ax5.bar(names_Robbery_Race, data_Robbery_Race)
+ax5.set_xlabel('Race')
+ax5.set_ylabel('Number of offenders')
+ax5.set_title('Robbery Race')
+
+#==================Robbery Age Data=======================
+
+df_Robbery_Age_Data = pd.read_csv('Offender-Age_05-28-2023_Robbery.csv', sep=',')
+df_Robbery_Age = pd.DataFrame(df_Robbery_Age_Data)
+names_Robbery_Age = df_Robbery_Age.iloc[:, 0]
+data_Robbery_Age = df_Robbery_Age.iloc[:, 1]
+
+fig6, ax6 = plt.subplots()
+ax6.bar(names_Robbery_Age, data_Robbery_Age)
+ax6.set_xlabel('Age')
+ax6.set_ylabel('Number of offenders')
+ax6.set_title('Robbery Age')
+
+#==================Aggravated Assault Age Data=======================
+
+df_Assault_Age_Data = pd.read_csv('Offender-Age_05-28-2023_Aggravated_Assault.csv', sep=',')
+df_Assault_Age = pd.DataFrame(df_Assault_Age_Data)
+names_Assault_Age = df_Assault_Age.iloc[:, 0]
+data_Assault_Age = df_Assault_Age.iloc[:, 1]
+fig7, ax7 = plt.subplots()
+ax7.bar(names_Assault_Age, data_Assault_Age)
+ax7.set_xlabel('Age')
+ax7.set_ylabel('Number of offenders')
+ax7.set_title('Aggravated assault Age')
+
+#==================Aggravated Assault Race Data=======================
+
+df_Assault_Race_Data = pd.read_csv('Offender-Race_05-28-2023_Aggravated_Assault.csv', sep=',')
+df_Assault_Race = pd.DataFrame(df_Assault_Race_Data)
+names_Assault_Race = df_Assault_Race.iloc[:, 0]
+data_Assault_Race = df_Assault_Race.iloc[:, 1]
+fig8, ax8 = plt.subplots()
+ax8.bar(names_Assault_Race, data_Assault_Race)
+ax8.set_xlabel('Race')
+ax8.set_ylabel('Number of offenders')
+ax8.set_title('Aggravated assault Race')
+
 plt.show()
-
-"""df_Homicide_Race = pd.DataFrame(df_Homicide_Race)
-df_Homicide_Race = df_Homicide_Race.apply(lambda x: x.astype(float) if x.name != 'key' else x)
-column_homicide_race = df_Homicide_Race.filter(like='value')"""
-#column_homicide_race_filtered = column_homicide_race[1:]
-
-
-#print(column_homicide_race_filtered)
-#C:/Users/xXx_420dankmeme_xXx/Desktop/PRG/python/Python_projekt/
 
 
